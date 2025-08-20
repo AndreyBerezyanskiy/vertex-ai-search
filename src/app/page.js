e6e6380a202b9d784a1a0046735aa7b0d701d10c
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2025-08-20 16:16:03 +0200

    test trigger

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Script from "next/script";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-1 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Script
             src="https://cloud.google.com/ai/gen-app-builder/client?hl=en_US"
