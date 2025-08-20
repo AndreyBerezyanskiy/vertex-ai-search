c88b6a99810dbcdfb6bc90b3b8c596f784fb13f8
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2025-08-20 16:12:06 +0200

    script tag

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <script src="https://cloud.google.com/ai/gen-app-builder/client?hl=en_US"></script>
+          <Script
+            src="https://cloud.google.com/ai/gen-app-builder/client?hl=en_US"
+            strategy="afterInteractive"
+          />
 
           <gen-search-widget
             configId="3cdcaeab-1ad1-464d-80f3-768e91b75bc6"
